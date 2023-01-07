3fe5f21b94b3159e16604c499f79a3417d5ef2ce
agon1121 <qqqq0www2@gmail.com>
2023-01-07 22:24:34 +0900

    ㅅㄴㅅㄴ

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 def sum(a, b):
-    print(a + b)
+    return a + b
 
-sum(1, 9)
+def mup(a, b):
+    for i in range(a):
+        b = sum(b, b)
+    return b
+def mup1(a, b):
+    for i in range(a):
+      b = mup(b , b)
+    return b
+
+print(mup1(3,2))
