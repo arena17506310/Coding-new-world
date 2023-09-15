cb56d069b776dda77cb7c369beb4532360fe24d8
whrua <kkuljaemhoneyjam@gmail.com>
2023-09-15 15:13:08 +0900

    update

--- account _reset.js
+++ account _reset.js
@@ -6,13 +6,13 @@ $(document).ready(function () {
 
     $(".message").removeClass("error success");
 
-    if (username === "" || password === "") {
-      $(".message").addClass("error").text("빈 칸에 요청하는 정보를 모두 기입 하여 주세요");
+    if (username === "") {
+      $(".message").addClass("error").text("빈 칸에 정보를 모두 기입 하여 주세요,");
       return;
     }
 
     // 여기서부터 서버와의 통신 등 추가 작업을 진행할 수 있습니다.
 
-    $(".message").addClass("success").text("회원가입 성공!");
+    $(".message").addClass("success").text("비번 초기화 요청 성공");
   });
 });
