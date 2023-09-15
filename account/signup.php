73009f154e8ee7d5aafa7ab90db0ab5bff9f77c8
agon1121 <qqqq0www2@gmail.com>
2023-09-15 22:59:02 +0900

    ㅇㅁㄴㅇㅁㄴ

--- account/signup.php
+++ account/signup.php
@@ -23,8 +23,12 @@ $stmt = $conn->prepare($sql);
 $stmt->bind_param("ssss", $username, $password, $phoneNum, $schoolNum);
 
 if ($stmt->execute()) {
-    echo "New record created successfully";
+    ob_start();
     echo("<script>location.replace('../board/main.html');</script>");
+    echo "New record created successfully";
+    header('Location: ccount.html');
+    echo("<script>location.replace('../board/main.html');</script>");
+    exit;
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
