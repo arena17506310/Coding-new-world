858506d0b99f5a044c9dda40753aefcbb5dc312c
whrua <kkuljaemhoneyjam@gmail.com>
2023-09-15 15:50:38 +0900

    windfsdf

--- accounrt/account _reset.js
+++ accounrt/account _reset.js
@@ -7,12 +7,25 @@ $(document).ready(function () {
     $(".message").removeClass("error success");
 
     if (username === "") {
-      $(".message").addClass("error").text("빈 칸에 정보를 모두 기입 하여 주세요,");
+      $(".message").addClass("error").text("빈 칸에 정보를 모두 기입하여 주세요.");
       return;
     }
 
-    // 여기서부터 서버와의 통신 등 추가 작업을 진행할 수 있습니다.
-
-    $(".message").addClass("success").text("비번 초기화 요청 성공");
+    // AJAX 요청 ( 백엔드가 건들면 ㅈ된다 개새기들아 )
+    $.ajax({
+      url: "signup.php",
+      type: "POST",
+      data: { username: username },
+      success: function (response) {
+        $(".message").addClass("success").text("비밀번호 초기화 요청 성공");
+        // 성공적으로 처리된 후 실행할 코드 작성 ( 백엔드가 건들면 ㅈ된다 개새기들아 )
+      },
+      error: function () {
+        $(".message")
+          .addClass("error")
+          .text("서버와의 통신 중 오류가 발생했습니다.");
+        // 오류 발생 시 실행할 코드 작성 ( 통신 에러 말고 건들면 죽인다 )
+      }
+    });
   });
 });
--- accounrt/account.js
+++ accounrt/account.js
@@ -2,17 +2,31 @@ $(document).ready(function () {
   $("#signup-form").submit(function (e) {
     e.preventDefault();
 
-    var username = $("#username").val();
+    var username = $("#username").val(); // username 필드 추가
+    var password = $("#password").val(); // password 필드 추가
 
     $(".message").removeClass("error success");
 
     if (username === "" || password === "") {
-      $(".message").addClass("error").text("빈 칸에 요청하는 정보를 모두 기입 하여 주세요");
+      $(".message").addClass("error").text("빈 칸에 정보를 모두 기입하여 주세요.");
       return;
     }
 
-    // 여기서부터 서버와의 통신 등 추가 작업을 진행할 수 있습니다.
-
-    $(".message").addClass("success").text("회원가입 성공!");
+    // AJAX 요청
+    $.ajax({
+      url: "signup.php",
+      type: "POST",
+      data: { username: username, password: password }, // 비밀번호 및 아이디 전송 ( 건들지 마라 뒤진다 )
+      success: function (response) {
+        $(".message").addClass("success").text("회원가입 요청 성공");
+        // 성공적으로 처리된 후 실행할 코드 작성
+      },
+      error: function () {
+        $(".message")
+          .addClass("error")
+          .text("서버와의 통신 중 오류가 발생했습니다.");
+        // 오류 발생 시 실행할 코드 작성
+      }
+    });
   });
 });
