d561fc21471a50a3120e9a059ee25c1ccd974346
Agon1121 <qqq0www2@gmail.com>
2023-09-15 15:33:14 +0900

    dsadsadsasa

--- accounrt/signup.php
+++ accounrt/signup.php
@@ -1,30 +1,7 @@
 <?php
-// DB 연결 설정
-$servername = "localhost";
-$username = "사용자명";
-$password = "비밀번호";
-$dbname = "데이터베이스명";
+  echo "DB연동 결과는?!<BR>";
+$pgsql = pg_Connect("host=svc.sel5.cloudtype.app:30544 dbname=template0 user=root password=camel99") or die('접속실패');
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("DB 연결 실패: " . $conn->connect_error);
-}
-
-// POST 요청으로부터 회원가입 폼 데이터 받기
-$username = $_POST["username"];
-$password = $_POST["password"];
-$phone_number = $_POST["phone_number"];
-$student_id = $_POST["student_id"];
-
-// SQL 쿼리 실행하여 데이터베이스에 회원정보 저장
-$sql = "INSERT INTO users (username, password, phone_number, student_id)
-        VALUES ('$username', '$password', '$phone_number', '$student_id')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "회원가입 성공!";
-} else {
-    echo "오류: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
+  $row = pg_fetch_array($pgsql);
+  echo "접속성공" .$row;
 ?>
