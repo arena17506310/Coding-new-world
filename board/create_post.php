8ead6734dba6e07f3f9ed7b03a7011ffd7a28f78
agon1121 <qqqq0www2@gmail.com>
2023-09-16 21:48:01 +0900

    51243125

---
+++ board/create_post.php
@@ -0,0 +1,31 @@
+<?php
+
+// Database connection details from signup.php
+
+$host = 'svc.sel5.cloudtype.app';
+$db   = 'alexandria';
+$user = 'root';
+$pass = 'camel99';
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+  die('Connection failed:'.$conn->connect_error);
+}
+
+$title = $_POST['title'];
+$content = $_POST['content'];
+
+$sql = "INSERT INTO posts(title,content) VALUES (?,?)";
+$stmt= $conn->prepare($sql);
+$stmt->bind_param("ss", $title,$content);
+
+if ($stmt->execute()) {
+  echo 'Post created successfully.';
+} else {
+  echo 'Error creating post.';
+}
+
+$conn->close();
+
+?>
---
+++ board/fetch_posts.php
@@ -0,0 +1,27 @@
+<?php
+
+$host = 'svc.sel5.cloudtype.app';
+$db   = 'alexandria';
+$user = 'root';
+$pass = 'camel99';
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+  die('Connection failed:'.$conn->connect_error);
+}
+
+$sql = "SELECT title,content FROM posts ORDER BY id DESC";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+    echo "<h3>" . $row["title"]. "</h3><p>" . $row["content"]. "</p><hr>";
+  }
+} else {
+  echo "No posts found.";
+}
+
+$conn->close();
+
+?>
