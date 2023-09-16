db730754482bc9d06be59d21ea5e5861a2f944a4
agon1121 <qqqq0www2@gmail.com>
2023-09-16 10:45:11 +0900

    ㅇㄴㅁㅇㅁㄴ

--- account/account.js
+++ account/account.js
@@ -27,16 +27,15 @@ $(document).ready(function () {
         schoolNum: schoolNum,
       }, // 비밀번호 및 아이디 전송 ( 건들지 마라 뒤진다 ) -> 전화번호랑 학번 저장 안함? 구별은 해야지
       success: function (response) {
-        $(".message").addClass("success").text("회원가입 요청 성공");
-        window.location.href = "signup.php";
-        console.log("success"); // 이 부분을 추가합니다.
-        // 성공적으로 처리된 후 실행할 코드 작성
-      },
-      error: function () {
-        $(".message")
-          .addClass("error")
-          .text("서버와의 통신 중 오류가 발생했습니다.");
-        // 오류 발생 시 실행할 코드 작성
+        if (response === "Success") {
+          // 가정: signup.php 에서 성공 시 'Success' 문자열 반환
+          $(".message").addClass("success").text("회원가입 요청 성공");
+          window.location.href = "/"; // 메인 페이지로 리다이렉트
+        } else {
+          $(".message")
+            .addClass("error")
+            .text(response + "\n서버와연결에실패했습니다."); // 실패 시 서버의 에러 메시지 출력
+        }
       },
     });
   });
