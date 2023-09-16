d4cea9d0b69ea32971b41119f6f328f7baede71f
agon1121 <qqqq0www2@gmail.com>
2023-09-16 11:12:55 +0900

    dada

---
+++ hello/login.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+  $("#login-form").submit(function (e) {
+    e.preventDefault();
+
+    var username = $("#username").val();
+    var password = $("#password").val();
+
+    $(".message").removeClass("error success");
+
+    // AJAX 요청
+    $.ajax({
+      url: "login.php",
+      type: "POST",
+      data: {
+        username: username,
+        password: password,
+      },
+      success: function (response) {
+        if (response.trim() === "Success") {
+          $(".message").addClass("success").text("로그인 성공");
+          window.location.href = "../board/main.html";
+        } else {
+          $(".message")
+            .addClass("error")
+            .text(response + "서버와 연결에 실패했습니다.");
+        }
+      },
+    });
+  });
+});
---
+++ hello/login.php
@@ -0,0 +1,39 @@
+<?php
+
+// Database connection details from signup.php
+
+$host = "svc.sel5.cloudtype.app:30279";
+$db   = "alexandria";
+$user = "root";
+$pass = "camel99";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if (!isset($_POST['username']) || !isset($_POST['password'])) {
+    die("Error: Required field is missing");
+}
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$sql = "SELECT pw FROM accounts WHERE userName=?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $username);
+
+$stmt->execute();
+$result = $stmt->get_result();
+$row = $result->fetch_assoc();
+
+if ($row && password_verify($password, $row['pw'])) {
+    echo 'Success';
+} else {
+    echo 'Invalid username or password';
+}
+
+$conn->close();
+
+?>
