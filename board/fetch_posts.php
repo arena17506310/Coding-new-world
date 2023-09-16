0f062c364bc739d1004091c4a041015b3aae46f1
agon1121 <qqqq0www2@gmail.com>
2023-09-16 22:05:47 +0900

    15412461

--- board/fetch_posts.php
+++ board/fetch_posts.php
@@ -11,17 +11,19 @@ if ($conn->connect_error) {
   die('Connection failed:'.$conn->connect_error);
 }
 
-$sql = "SELECT title,content FROM posts ORDER BY id DESC";
-$result = $conn->query($sql);
+$sql = "SELECT id, title FROM posts ORDER BY id DESC";
+$result= $conn ->query($sql);
 
-if ($result->num_rows > 0) {
-  while($row = $result->fetch_assoc()) {
-    echo "<h3>" . $row["title"]. "</h3><p>" . $row["content"]. "</p><hr>";
-  }
-} else {
-  echo "No posts found.";
+if ($result ->num_rows >0) {
+ while($row= $result ->fetch_assoc()) {
+
+   // 게시물 제목을 하이퍼링크로 출력
+   echo "<a href='post_detail.php?id=".$row["id"]."'>".$row["title"]."</a><br>";
+ }
+} else{
+ echo"No posts found.";
 }
 
-$conn->close();
+$conn ->close();
 
 ?>
